137937e2cd27e50e5f2dac48f0191a589b33b0ed
Alexis Torrado <sgmr2027@gmail.com>
2026-05-10 00:23:55 -0500

    continuando debug rack

--- inventory/urls.py
+++ inventory/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 app_name = 'inventory'
-urlpatterns = [
-    path('api/rack/<str:id_qr>/', views.rack_by_qr, name='rack_by_qr'),
-]
+# urlpatterns = [
+#     path('api/rack/<str:id_qr>/', views.rack_by_qr, name='rack_by_qr'),
+# ]
+urlpatterns = []
